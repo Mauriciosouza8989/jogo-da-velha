f5eb80cce734503bd40c04453f2c89221ff35fb1
Maurício <72310791+Mauriciosouza8989@users.noreply.github.com>
2022-11-27 14:25:31 -0300

    Update game.js

--- javascript/game.js
+++ javascript/game.js
@@ -3,75 +3,63 @@ let playerTime = 0;
 let symbols = ['X', 'O'];
 let gameOver = false;
 let empate = false;
+let x = 0;
+let o = 0
 
 
-function handleClick() {
 
 
-    if (playerTime == 0 && gameOver != true) {
-        if (this.innerHTML == '') {
-            this.innerHTML = `<div class="x rm"></div>`;
-            board[this.id] = 'x';
-            checkWin();
-            if (gameOver == true) {
-                mostrador.innerHTML = `O jogador "${symbols[playerTime]}" venceu o Jogo.`
-            } else {
-                checkempate();
-                if (empate == true) {
-                    mostrador.innerHTML = 'Opa, deu empate';
-                    empate = false;
-                } else {
-                    playerTime = 1;
-                    mostrador.innerHTML = `É a vez do jogador <h1>${symbols[playerTime]}</h1>`;
-                }
-            }
-        }
-    } else {
-        if (this.innerHTML == '' && gameOver != true) {
-            this.innerHTML = `<div class="o rm"></div>`;
-            board[this.id] = 'o';
-            checkWin();
-            if (gameOver == true) {
-                mostrador.innerHTML = `O jogador "${symbols[playerTime]}" venceu o Jogo.`
-            } else {
-                checkempate();
-                if (empate == true) {
-                    mostrador.innerHTML = 'Opa, deu empate';
-                    empate = false;
-                } else {
-                    playerTime = 0;
-                    mostrador.innerHTML = `É a vez do jogador <h1>${symbols[playerTime]}</h1>`;
-                }
-            }
-        }
-    }
-}
+
 
 
 function checkWin() {
     if (board[0] == board[1] && board[1] == board[2] && board[0] != '') {
         gameOver = true;
+        playerTime == 0 ? x += 1 : o += 1;
+        jogadorO.innerHTML = o;
+        jogadorX.innerHTML = x
         reiniciar.style.display = "initial";
     } else if (board[3] == board[4] && board[4] == board[5] && board[3] != '') {
         gameOver = true;
+        playerTime == 0 ? x += 1 : o += 1;
+        jogadorO.innerHTML = o;
+        jogadorX.innerHTML = x
         reiniciar.style.display = "initial";
     } else if (board[6] == board[7] && board[7] == board[8] && board[6] != '') {
         gameOver = true;
+        playerTime == 0 ? x += 1 : o += 1;
+        jogadorO.innerHTML = o;
+        jogadorX.innerHTML = x
         reiniciar.style.display = "initial";
     } else if (board[0] == board[3] && board[3] == board[6] && board[0] != '') {
         gameOver = true;
+        playerTime == 0 ? x += 1 : o += 1;
+        jogadorO.innerHTML = o;
+        jogadorX.innerHTML = x
         reiniciar.style.display = "initial";
     } else if (board[1] == board[4] && board[4] == board[7] && board[1] != '') {
         gameOver = true;
+        playerTime == 0 ? x += 1 : o += 1;
+        jogadorO.innerHTML = o;
+        jogadorX.innerHTML = x
         reiniciar.style.display = "initial";
     } else if (board[2] == board[5] && board[5] == board[8] && board[2] != '') {
         gameOver = true;
+        playerTime == 0 ? x += 1 : o += 1;
+        jogadorO.innerHTML = o;
+        jogadorX.innerHTML = x
         reiniciar.style.display = "initial";
     } else if (board[0] == board[4] && board[4] == board[8] && board[0] != '') {
         gameOver = true;
+        playerTime == 0 ? x += 1 : o += 1;
+        jogadorO.innerHTML = o;
+        jogadorX.innerHTML = x
         reiniciar.style.display = "initial";
     } else if (board[2] == board[4] && board[4] == board[6] && board[2] != '') {
         gameOver = true;
+        playerTime == 0 ? x += 1 : o += 1;
+        jogadorO.innerHTML = o;
+        jogadorX.innerHTML = x
         reiniciar.style.display = "initial";
     }
 }
