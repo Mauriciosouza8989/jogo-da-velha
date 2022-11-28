f78623c3fef1b571ba016d07cd9ec1023b6e05eb
Maurício <72310791+Mauriciosouza8989@users.noreply.github.com>
2022-11-28 20:05:43 -0300

    adicionada a funcao

--- javascript/game.js
+++ javascript/game.js
@@ -8,59 +8,40 @@ let o = 0
 
 
 
-
+function pontuation() {
+    playerTime == 0 ? x += 1 : o += 1;
+    jogadorO.innerHTML = o;
+    jogadorX.innerHTML = x
+    reiniciar.style.display = "initial";
+}
 
 
 
 function checkWin() {
     if (board[0] == board[1] && board[1] == board[2] && board[0] != '') {
         gameOver = true;
-        playerTime == 0 ? x += 1 : o += 1;
-        jogadorO.innerHTML = o;
-        jogadorX.innerHTML = x
-        reiniciar.style.display = "initial";
+        pontuation()
     } else if (board[3] == board[4] && board[4] == board[5] && board[3] != '') {
         gameOver = true;
-        playerTime == 0 ? x += 1 : o += 1;
-        jogadorO.innerHTML = o;
-        jogadorX.innerHTML = x
-        reiniciar.style.display = "initial";
+        pontuation()
     } else if (board[6] == board[7] && board[7] == board[8] && board[6] != '') {
         gameOver = true;
-        playerTime == 0 ? x += 1 : o += 1;
-        jogadorO.innerHTML = o;
-        jogadorX.innerHTML = x
-        reiniciar.style.display = "initial";
+        pontuation()
     } else if (board[0] == board[3] && board[3] == board[6] && board[0] != '') {
         gameOver = true;
-        playerTime == 0 ? x += 1 : o += 1;
-        jogadorO.innerHTML = o;
-        jogadorX.innerHTML = x
-        reiniciar.style.display = "initial";
+        pontuation()
     } else if (board[1] == board[4] && board[4] == board[7] && board[1] != '') {
         gameOver = true;
-        playerTime == 0 ? x += 1 : o += 1;
-        jogadorO.innerHTML = o;
-        jogadorX.innerHTML = x
-        reiniciar.style.display = "initial";
+        pontuation()
     } else if (board[2] == board[5] && board[5] == board[8] && board[2] != '') {
         gameOver = true;
-        playerTime == 0 ? x += 1 : o += 1;
-        jogadorO.innerHTML = o;
-        jogadorX.innerHTML = x
-        reiniciar.style.display = "initial";
+        pontuation()
     } else if (board[0] == board[4] && board[4] == board[8] && board[0] != '') {
         gameOver = true;
-        playerTime == 0 ? x += 1 : o += 1;
-        jogadorO.innerHTML = o;
-        jogadorX.innerHTML = x
-        reiniciar.style.display = "initial";
+        pontuation()
     } else if (board[2] == board[4] && board[4] == board[6] && board[2] != '') {
         gameOver = true;
-        playerTime == 0 ? x += 1 : o += 1;
-        jogadorO.innerHTML = o;
-        jogadorX.innerHTML = x
-        reiniciar.style.display = "initial";
+        pontuation()
     }
 }
 
